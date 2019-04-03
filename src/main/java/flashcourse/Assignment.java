029585bc73b16cf9471c77ce186784fdd2ed731f
Ryan <rbohorqu@asu.edu>
2019-04-03 14:02:32 -0700

    Update Assignment.java to actually extend from Task
    This commit simply moves most of the functionality from Assignment to the abstract class task, and gets much smaller as a result

--- src/main/java/flashcourse/Assignment.java
+++ src/main/java/flashcourse/Assignment.java
@@ -3,20 +3,23 @@
  * Author: Ryan Bohorquez
  * Date:4/3/2019
  * Description: This file is used for the creation and storing of assignments. The assignment class holds all the
- *  information for a class, and the ASSIGNEDGROUP enum is used determine who an assignment is for.
+ *  information for a class, and the ASSIGNEDGROUP enum is used determine who an assignment is for. 
  */
 
 package main.java.flashcourse;
 
 import main.java.memoranda.date.*;
 
-public class Assignment {
+/**
+ * Class: Assignment
+ * Description: Assignment extends all the functionality from Task and adds the ability to store a course
+ *  as well as modify its value. By default an assignment needs to be created with a course in mind, but it
+ *  can be removed and have null for its course.
+ */
+public class Assignment extends Task{
 
 	//Member variables
 	private Course course;
-	private CalendarDate dueDate;
-	private ASSIGNEDGROUP assignedGroup;
-	private String name;
 	
 	/*
 	 *Constructor for the Assignment class, intializes local variables based on parameters and calls addAssignment on the passed course
@@ -26,11 +29,9 @@ public class Assignment {
 	 *@param assignedGroup the ASSIGNEDGROUP this task is assigned to
 	 *@param name the name of this assignment for printing purposes.
 	 */
-	public Assignment(Course course, CalendarDate dueDate, ASSIGNEDGROUP assignedGroup, String name) {
+	public Assignment(Course course, CalendarDate dueDate, ASSIGNEDGROUP assignedGroup, String name, String description) {
+		super(dueDate, assignedGroup, name, description);
 		course.addAssignment(this);
-		this.dueDate = dueDate;
-		this.assignedGroup = assignedGroup;
-		this.name = name;
 	}
 	
 	public Course getCourse() {
@@ -42,30 +43,6 @@ public class Assignment {
 		this.removeCourse();
 		this.course = newCourse;
 		newCourse.addAssignment(this);
-	}
-	
-	public CalendarDate getDueDate() {
-		return this.dueDate;
-	}
-	
-	public void setDueDate(CalendarDate date) {
-		this.dueDate = date;
-	}
-	
-	public ASSIGNEDGROUP getAssignedGroup() {
-		return this.assignedGroup;
-	}
-	
-	public void setAssignedGroup(ASSIGNEDGROUP group) {
-		this.assignedGroup = group;
-	}
-	
-	public String getName() {
-		return this.name;
-	}
-	
-	public void setName(String newName) {
-		this.name = newName;
 	}
 	
 	/*
